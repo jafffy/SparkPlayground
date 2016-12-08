b3db49ed5c5a72044762701ba1d31806ecd7ff31
Jaewon Choi <jainersoer@gmail.com>
2016-12-08 15:17:55 +0900

    Run simple spark code

--- src/main/scala/edu/ajou/jafffy/SimpleApp.scala
+++ src/main/scala/edu/ajou/jafffy/SimpleApp.scala
@@ -1,7 +1,20 @@
 package edu.ajou.jafffy
 
+import org.apache.spark.SparkContext
+import org.apache.spark.SparkContext._
+import org.apache.spark.SparkConf
+
 object SimpleApp {
   def main(args: Array[String]) : Unit = {
-    println("Hello, World!")
+    val logFile = "README.md"
+    val conf = new SparkConf()
+      .setAppName("Simple Application")
+      .setMaster("local[2]")
+    val sc = new SparkContext(conf)
+    val logData = sc.textFile(logFile, 2).cache()
+    val numAs = logData.filter(line => line.contains("a")).count()
+    val numBs = logData.filter(line => line.contains("b")).count()
+    println(s"Lines with a: $numAs, Lines with b: $numBs")
+    sc.stop()
   }
 }
